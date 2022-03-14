95a69474e2dbb3fb6359b913849ce2487d12e2d9
Shaheer <sm_shaheer@msn.com>
2022-03-14 12:05:23 +0500

    Updates

---
+++ precache-manifest.43b62e76cae721ac6400d28286c40f4b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebc6abcbbf3415cc9314f4a5aca1ccca",
+    "url": "/color-gen/index.html"
+  },
+  {
+    "revision": "8ef3799592c5c1173647",
+    "url": "/color-gen/static/css/main.9f3b6256.chunk.css"
+  },
+  {
+    "revision": "cf6bca8e816ecf72b2d4",
+    "url": "/color-gen/static/js/2.11f691c1.chunk.js"
+  },
+  {
+    "revision": "1dbab337b90e8f2eb0a4a79d2378f49b",
+    "url": "/color-gen/static/js/2.11f691c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ef3799592c5c1173647",
+    "url": "/color-gen/static/js/main.542afb87.chunk.js"
+  },
+  {
+    "revision": "b02566d15d244a921362",
+    "url": "/color-gen/static/js/runtime-main.78ace6f6.js"
+  }
+]);
